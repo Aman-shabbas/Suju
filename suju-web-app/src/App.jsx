afd95db9b40fc7a99fc86da61658f19bc2f567c2
sujoy <sujoychoolur@gmail.com>
2025-08-12 11:01:19 +0530

    [#3/sujoy] updated the UI with stateManagement

--- suju-web-app/src/App.jsx
+++ suju-web-app/src/App.jsx
@@ -1,16 +1,64 @@
-import { useState } from "react";
+import { useState, React } from "react";
 import "./App.css";
 
 const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState("");
+
   return (
     <>
       <div id="main-box">
         <div id="message-area">
-          <h1>Chat App</h1>
+          {/* <div className="message">
+            {messages.map((msg, ind) => (
+              <>
+                <div key={ind} className="query-box">
+                  <p key={ind} className="query">
+                    {msg.query}
+                  </p>
+                </div>
+                <div key={ind} className="response-box">
+                  <p key={ind} className="resp">
+                    {msg.response}
+                  </p>
+                </div>
+              </>
+            ))}
+          </div> */}
+
+          <div className="message">
+            {messages.map((msg, ind) => (
+              <div key={ind}>
+                <div className="query-box">
+                  <p className="query">{msg.query}</p>
+                </div>
+                <div className="response-box">
+                  <p className="resp">{msg.response}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <div className="textarea">
-          <input type="text" placeholder="Message..." id="input" />
-          <button type="submit" id="submit-button">
+          <input
+            type="text"
+            placeholder="Message..."
+            id="input"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button
+            type="submit"
+            id="submit-button"
+            onClick={() => {
+              if (value.trim() === "") return;
+              setMessages([
+                ...messages,
+                { query: value, response: "This is a mock response" },
+              ]);
+              setValue("");
+            }}
+          >
             Submit
           </button>
         </div>
