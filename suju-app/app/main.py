b1eeed3dde5eec910116295c010d36ea5815b8da
djkc <dhanojkc123@gmail.com>
2025-08-14 10:27:33 +0530

    [#4/Dhanoj] Resolve the CORS problem

--- suju-app/app/main.py
+++ suju-app/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.routers import chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Chat Lookup API")
+
+# Set up CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"]  # Allows all methods
+    allow_headers=["*"]  # Allows all headers
+)
+
 app.include_router(chat.router)
 
 @app.get("/")
